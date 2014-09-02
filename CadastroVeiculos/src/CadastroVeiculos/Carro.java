4bbf0df410488291c8330416a5f9abf4a694f838
iesb <iesb@iesb-ThinkCentre-M58e.(none)>
2014-09-01 21:31:58 -0300

    Corrigindo

---
+++ CadastroVeiculos/src/CadastroVeiculos/Carro.java
@@ -0,0 +1,60 @@
+package CadastroVeiculos;
+
+public class Carro {
+	private String modelo;
+	private String cor;
+	private String placa;
+	private String proprietario;
+	private String contato;
+	private int ano;
+
+	public String getModelo() {
+		return modelo;
+
+	}
+
+	public String getCor() {
+		return cor;
+	}
+
+	public String getPlaca() {
+		return placa;
+	}
+
+	public String getProprietario() {
+		return proprietario;
+	}
+
+	public String getcontato() {
+		return contato;
+	}
+
+	public int getAno() {
+		return ano;
+	}
+
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+
+	public void setCor(String cor) {
+		this.cor = cor;
+	}
+
+	public void setPlaca(String placa) {
+		this.placa = placa;
+	}
+
+	public void setProprietario(String proprietario) {
+		this.proprietario = proprietario;
+	}
+
+	public void setContato(String contato) {
+		this.contato = contato;
+	}
+
+	public void setAno(int ano) {
+		this.ano = ano;
+	}
+
+}
